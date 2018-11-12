d50fce59420e8d791bf971f9ec9b9732a18242f7
Joao Emidio <joao.emidio@2isa.org>
2018-11-12 10:24:16 +0100

    Ajoutez des fichiers projet.

---
+++ Jeu2Des/Classement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jeu2Des
+{
+    public class Classement
+    {
+        public Classement()
+        {
+
+        }
+    }
+}
